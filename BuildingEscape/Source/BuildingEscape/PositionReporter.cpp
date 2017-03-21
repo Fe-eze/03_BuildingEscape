24520cfae8d7975cd1b4cb0b554af20b37fe231e
Fe-eze <fenobus@gmail.com>
2017-03-21 17:39:30 +0000

    Staging after first Pointer madness!

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -20,7 +20,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position reporter reporting for duty!"));
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position reporter for %s reporting for duty!"), *ObjectName);
 	
 }
 
